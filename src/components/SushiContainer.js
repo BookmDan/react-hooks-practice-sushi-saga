525dd91cc1ae3f96650b68807ff92010d140861f
BookmDan <danoh329@gmail.com>
2023-10-29 09:14:37 -0700

    sushi

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
-function SushiContainer(props) {
+function SushiContainer({sushis}) {
   return (
     <div className="belt">
+      {sushis.map(sushi=> <Sushi sushi={sushi} />)}
       {/* Render Sushi components here! */}
       <MoreButton />
     </div>
